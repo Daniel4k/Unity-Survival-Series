063b2dc5db49581f25e3c5c998dc0e42741d61a5
TheCGMaster <cgmasteremail@gmail.com>
2016-02-17 13:00:34 -0500

    Changing The Code

--- Assets/InventoryManager.cs
+++ Assets/InventoryManager.cs
@@ -2,29 +2,47 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 public class InventoryManager : MonoBehaviour {
-	public int NextSlot = 1;
+	public int NextSlot = 0;
+	public GameObject FullText;
 	public List<GameObject> Items = new List<GameObject>();
 	public void Update()
 	{
-
-		if(Input.GetMouseButton(0))
+					if(GameObject.Find("Slot" + "0").gameObject.GetComponent<Slot>().isAvailable == true)
+						{
+							FullText.SetActive(false);
+						}	
+		if(NextSlot == 12)
+			{
+					NextSlot = 0;
+			}
+		if(Input.GetMouseButtonDown(0))
 		{
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
-			if(Physics.Raycast(ray, out hit, 100))
+			if(Physics.Raycast(ray, out hit, 10))
 			{
 				if(hit.transform.gameObject.tag == "Item")
 				{
-					Items.Add(hit.transform.gameObject);
-					GameObject.Find("Slot" + NextSlot).gameObject.GetComponent<Slot>().SlotFunction(hit.transform.gameObject.GetComponent<Item>().Image, hit.transform.gameObject.GetComponent<Item>().Weight);
-					hit.transform.parent = this.transform;
-					hit.transform.position = this.transform.position;
-					hit.transform.gameObject.SetActive(false);
+					if(GameObject.Find("Slot" + "11").gameObject.GetComponent<Slot>().isAvailable == false && GameObject.Find("Slot" + "1").gameObject.GetComponent<Slot>().isAvailable == false)
+						{
+							FullText.SetActive(true);
+						}else
+						{
+					if(GameObject.Find("Slot" + NextSlot).gameObject.GetComponent<Slot>().isAvailable)
+					{
+						Items.Add(hit.transform.gameObject);
+						GameObject.Find("Slot" + NextSlot).gameObject.GetComponent<Slot>().SlotFunction(hit.transform.gameObject.GetComponent<Item>().Image, hit.transform.gameObject.GetComponent<Item>().Weight);
+						hit.transform.parent = this.transform;
+						hit.transform.position = this.transform.position;
+						hit.transform.gameObject.SetActive(false);
+					}
+					else
+					{
+						NextSlot += 1;
+					}
+				}
 				}
 			}
-
-
-
 		}
 	}
 }
--- Assets/Slot.cs
+++ Assets/Slot.cs
@@ -5,17 +5,46 @@ public class Slot : MonoBehaviour {
 
 
 	public Sprite Icon;
+	public Sprite Origanal;
+	public int Number;
 	public int Weight;
+	public bool isAvailable = true;
 	public void Update()
 	{
-
+		if(isAvailable == true)
+		{
+			this.GetComponent<Button>().enabled = false;
+		}
+		if(isAvailable == false)
+		{
+			this.GetComponent<Button>().enabled = true;
+		}
 
 	}
 	public void SlotFunction(Sprite Icon, int Weight)
 	{
-
+		Number = GameObject.Find("Main Camera").gameObject.GetComponent<InventoryManager>().Items.Count - 1;
 		this.gameObject.GetComponent<Image>().overrideSprite = Icon;
 		//Change
 		GameObject.Find("Main Camera").gameObject.GetComponent<InventoryManager>().NextSlot += 1;
+		isAvailable = false;
 	}
+	public void RemoveItem()
+	{
+		GameObject.Find("Main Camera").gameObject.GetComponent<InventoryManager>().Items[Number].transform.parent = null;
+		GameObject.Find("Main Camera").gameObject.GetComponent<InventoryManager>().Items[Number].transform.position =  new Vector3(GameObject.Find("Main Camera").gameObject.GetComponent<InventoryManager>().Items[Number].transform.position.x * 2, GameObject.Find("Main Camera").gameObject.GetComponent<InventoryManager>().Items[Number].transform.position.y,GameObject.Find("Main Camera").gameObject.GetComponent<InventoryManager>().Items[Number].transform.position.z);
+		GameObject.Find("Main Camera").gameObject.GetComponent<InventoryManager>().Items[Number].gameObject.SetActive(true);
+		Slot[] slots = FindObjectsOfType(typeof(Slot)) as Slot[];
+		foreach(Slot slot in slots)
+		{
+			if(slot.Number != 0)
+			{
+				slot.Number -= 1;
+			}
+		}
+			this.gameObject.GetComponent<Image>().overrideSprite = Origanal;
+			GameObject.Find("Main Camera").gameObject.GetComponent<InventoryManager>().Items.RemoveAt(Number);
+			isAvailable = true;
+		}
+
 }
